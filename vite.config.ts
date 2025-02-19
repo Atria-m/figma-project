db13b93816ff588137ad94d736d1259217c52d04
Atria <atriamoiedy13@gmail.com>
2025-02-19 06:21:33 -0800

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base:"./"
+  base: "./",  // باید مسیر پایه رو برای ریشه سایت تنظیم کنی
+  build: {
+    outDir: "dist",  // فایل‌ها به پوشه dist میرن
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "about.html", // اگه صفحه‌های دیگه داری
+        contact: "pricing.html",
+      },
+    },
+  },
 });
